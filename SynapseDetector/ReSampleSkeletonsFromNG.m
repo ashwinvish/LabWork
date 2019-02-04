f15f16eca5ae70ba70f4e4a0ab47dab8e7e71b45
ashwin <ashwin@nat-oitwireless-inside-vapornet100-10-8-249-228.princeton.edu>
2019-02-04 15:23:16 -0500

    Made compatible with both systems

--- SynapseDetector/ReSampleSkeletonsFromNG.m
+++ SynapseDetector/ReSampleSkeletonsFromNG.m
@@ -1,19 +1,26 @@
-
-filePath  = '/Users/ashwin/Documents/LowEMtoHighEM/SWC_all/consensus-20180920/swc/';
-cd(filePath);
-D = dir('*.swc');
-
-parfor i = 1:size(D,1)
-    if (strfind(D(i).name,'.') == 6)
-        fileName = sprintf('%s',D(i).name);
-        fprintf('processing files : %s', fileName)
-        [tree,~,~] = load_tree(fullfile(filePath,fileName));
-        reSampleFactor = 5000;
-        tree = resample_tree(tree,reSampleFactor);
-        newFileName = sprintf('%s_reSample_%d.swc',D(i).name,reSampleFactor);
-        swc_tree(tree, fullfile(filePath,newFileName));t
-    else
-        continue;
-    end
-    
+clc;
+clear;
+if ismac
+    addpath(genpath('/Users/ashwin/Documents/LabWork'));
+    filePath  = '/Users/ashwin/Documents/LowEMtoHighEM/SWC_all/consensus-20180920/swc/';
+else
+    addpath(genpath('/usr/people/ashwinv/seungmount/research/Ashwin/Scripts'));
+    filepath = '/usr/people/ashwinv/seungmount/research/Ashwin/Z-Brain/LowEMtoHighEM/SWC_all/consensus-20180920/swc/'
 end
+    cd(filePath);
+    D = dir('*.swc');
+    
+    parfor i = 1:size(D,1)
+        if (strfind(D(i).name,'.') == 6)
+            fileName = sprintf('%s',D(i).name);
+            fprintf('processing files : %s', fileName)
+            [tree,~,~] = load_tree(fullfile(filePath,fileName));
+            reSampleFactor = 5000;
+            tree = resample_tree(tree,reSampleFactor);
+            newFileName = sprintf('%s_reSample_%d.swc',D(i).name,reSampleFactor);
+            swc_tree(tree, fullfile(filePath,newFileName));t
+        else
+            continue;
+        end
+        
+    end
