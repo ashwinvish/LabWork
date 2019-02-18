2c75c089efa9b371e5d59d124564392038564cb4
ashwin <ashwin@nat-oitwireless-inside-vapornet100-10-9-91-250.princeton.edu>
2019-02-18 17:15:39 -0500

    minor changes

--- SynapseDetector/PathLengthToCoordinate.m
+++ SynapseDetector/PathLengthToCoordinate.m
@@ -1,11 +1,14 @@
-function [pathLength] = PathLengthToCoordinate(A,inTree)
+function [pathLength] = PathLengthToCoordinate(pointCloud,inTree)
+%% Calculate pathlength from root node to A
+% A is mx3 vector
+% inTree is the tree structure associated with that A.
+
 B = [inTree.X,inTree.Y,inTree.Z];
 len = [0;len_tree(inTree)];
 Idpar = ipar_tree(inTree);
-
-pathLength = zeros(size(A,1),1);
-for i = 1:size(A,1)
-    [k,~] = dsearchn(B,A(i,:));
+pathLength = zeros(size(pointCloud,1),1);
+for i = 1:size(pointCloud,1)
+    [k,~] = dsearchn(B,pointCloud(i,:));
     pathLength(i) = sum(len(Idpar(k,:)+1));
 end
 end
