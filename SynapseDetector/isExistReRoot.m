742815ac8536bd2e90aef2c3338bb0c4578643ba
ashwin <ashwin@nat-oitwireless-inside-vapornet100-10-9-203-218.princeton.edu>
2019-02-19 14:32:23 -0500

    checks to see if cell has been reRooted.

---
+++ SynapseDetector/isExistReRoot.m
@@ -0,0 +1,16 @@
+function [logical] = isExistReRoot(cellID)
+% isExistReRoot checks if cellID has be reRooted 
+
+filePath  = '/Users/ashwin/Documents/LowEMtoHighEM/SWC_all/consensus-20180920/swc/';
+reSampleFactor = 5000;
+
+for i = 1:length(cellID)
+    fileName = sprintf('%d_reRoot_reSample_%d.swc',cellID(i),reSampleFactor);
+    if exist (fullfile(filePath, fileName))
+        logical(i) = true;
+    else
+        logical(i) = false;
+    end
+end
+
+end
