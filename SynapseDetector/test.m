bf2e002cf7102bfc822a27a74022fa56b5670066
ashwin <ashwin@nat-oitwireless-inside-vapornet100-10-9-48-76.princeton.edu>
2019-02-25 17:08:38 -0500

    minor changes

--- SynapseDetector/test.m
+++ SynapseDetector/test.m
@@ -1,13 +1,43 @@
-for i = 1:numel(ABDr_CellIDs)
-    ABDr(i) = InputsByClass(ABDr_CellIDs(i),df);
-    filename = sprintf('%d.swc',ABDr_CellIDs(i));
-    if exist(fullfile(fname,filename))>0
-        temp = dlmread(fullfile(fname,filename), ' ');
-        coord = temp(:,3:5);
-        ABDr(i).Origin = TransformPoints(coord(1,:),4); 
-    else 
-        ABRr(i).Origin = NaN;
+for i = 1:size(ContraRhombomere,2)
+    A(i,:) = struct2array(ContraRhombomere(i));
+end
+
+index = 1
+for i = 1:size(ContraRhombomere,2)
+    if find(A(i,2:end)) == 1
+        r3Contra(index) = A(i,1);
+        index = index+1;
     end
-    clear coord;
-    clear temp;
+end
+
+index = 1
+for i = 1:size(ContraRhombomere,2)
+    if find(A(i,2:end)) == 2
+        r4Contra(index) = A(i,1);
+        index = index+1;
+    end
+end
+
+index = 1
+for i = 1:size(ContraRhombomere,2)
+    if find(A(i,2:end)) == 3
+        r5Contra(index) = A(i,1);
+        index = index+1;
+    end
+end
+
+index = 1
+for i = 1:size(ContraRhombomere,2)
+    if find(A(i,2:end)) == 4
+        r6Contra(index) = A(i,1);
+        index = index+1;
+    end
+end
+
+index = 1
+for i = 1:size(ContraRhombomere,2)
+    if find(A(i,2:end)) == 5
+        r7Contra(index) = A(i,1);
+        index = index+1;
+    end
 end
