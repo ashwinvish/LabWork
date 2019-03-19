86ff4f1b17d5e618a6bc8f04adbb51a0b1fd5d61
ashwin <ashwin@av-dongle.princeton.edu>
2019-03-19 15:50:06 -0400

    Deconvolved the Ca activity based on Daie et. al

---
+++ SynapseDetector/DeconvCa.m
@@ -0,0 +1,18 @@
+function [dstaf] = DeconvCa(staf)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+delt = 0.05;
+tau_cirf = 1.9;
+
+dlen = floor(5/delt);           % length of kernel (in index units)
+Tnew = linspace(0,dlen*delt,dlen);
+ker = exp(-(Tnew-Tnew(1))/tau_cirf);
+zpad = zeros(dlen-1,1);         % zero-padding
+
+
+dstaf = (tau_cirf/delt)*deconv([staf;zpad],ker);
+dstaf = medfilt1(dstaf,5);
+
+end
+
